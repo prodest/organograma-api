a6b45e2da09fa5f7acce62b417f27b1174776be7
alvarollmenezes <alvarollmenezes@gmail.com>
2016-09-26 16:39:29 -0300

    Adiciona integração com travis

--- .develop.sh
+++ .develop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
-docker build -t $DOCKER_IMAGE-dev -f ./Dockerfile .
+docker build -t $DOCKER_IMAGE-dev .
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_IMAGE-dev
 
-export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/9x99999 #env organograma (9x99999)
-export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/9x99999/environments/9x999/composeconfig #stack dev (9x999)
+export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/1a10541 #env processoeletronico (1a10541)
+export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/1a10541/environments/1e100/composeconfig #stack dev (1e100)
 
 git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
--- .travis.sh
+++ .travis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-docker build -t $DOCKER_IMAGE -f ./Dockerfile .
+docker build -t $DOCKER_IMAGE .
