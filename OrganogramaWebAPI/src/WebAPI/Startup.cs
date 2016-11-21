d25e2289171208403599a9d55af8de3b69af1c39
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2016-11-21 16:00:19 -0200

    Atualização das bibliotescas do Nuget. Configuração do HelpPages.

--- OrganogramaWebAPI/src/WebAPI/Startup.cs
+++ OrganogramaWebAPI/src/WebAPI/Startup.cs
@@ -4,9 +4,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
 using Newtonsoft.Json;
 using Organograma.WebAPI.Config;
+using Swashbuckle.Swagger.Model;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 
 namespace Organograma.WebAPI
 {
@@ -42,6 +45,30 @@ namespace Organograma.WebAPI
 
             // Inject an implementation of ISwaggerProvider with defaulted settings applied
             services.AddSwaggerGen();
+
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Organograma Web API",
+                    Description = "Núcleo de serviço do sistema Organograma implementado pelo Governo do Estado do Espírito Santo.",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "PRODEST",
+                        Email = "atendimento@prodest.es.gov.br",
+                        Url = "http://prodest.es.gov.br"
+                    }
+                });
+
+                //Determine base path for the application.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                //Set the comments path for the swagger json and ui.
+                var xmlPath = Path.Combine(basePath, "WebAPI.xml");
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,7 +97,7 @@ namespace Organograma.WebAPI
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
-            app.UseSwaggerUi();
+            app.UseSwaggerUi("api/documentation");
         }
     }
 }
