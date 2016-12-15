1bdda0ed7f186c9dc4c99808c6f6a7264bdb0532
alvarollmenezes <alvarollmenezes@gmail.com>
2016-12-15 11:22:37 -0200

    Corrige (de novo) os scripts de integração

--- scripts/.master.sh
+++ scripts/.master.sh
@@ -5,16 +5,16 @@ docker build -t $DOCKER_IMAGE -f ./Dockerfile .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_IMAGE
 
-export RANCHER_ENV_ID=1a10541 #env processoeletronico (1a10541)
+export RANCHER_ENV_ID=1a10541 #env SEP/organograma (1a10541)
 export RANCHER_STACK_ID=1e231 #stack prd (1e231)
 export RANCHER_STACK=prd #stack prd (1e231)
 
 export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID
 export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID/environments/$RANCHER_STACK_ID/composeconfig
-export RANCHER_SERVICE_NAME=processoeletronico-api
+
 
 #Atualiza a infra
 git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
 npm install
-node ./client $RANCHER_SERVICE_NAME 40000
+node ./client $DOCKER_IMAGE 40000
--- scripts/.release.sh
+++ scripts/.release.sh
@@ -5,16 +5,15 @@ docker build -t $DOCKER_IMAGE-hmg -f ./Dockerfile .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $DOCKER_IMAGE-hmg
 
-export RANCHER_ENV_ID=1a10541 #env processoeletronico (1a10541)
+export RANCHER_ENV_ID=1a10541 #env SEP/organograma (1a10541)
 export RANCHER_STACK_ID=1e102 #stack hmg (1e102)
 export RANCHER_STACK=hmg #stack hmg (1e102)
 
 export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID
 export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID/environments/$RANCHER_STACK_ID/composeconfig
-export RANCHER_SERVICE_NAME=processoeletronico-api
 
 #Atualiza a infra
 git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
 npm install
-node ./client $RANCHER_SERVICE_NAME 40000
+node ./client $DOCKER_IMAGE 40000
