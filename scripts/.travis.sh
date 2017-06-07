0ed51cfdae973dec319a2261c273c660a24f239f
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-06-07 11:26:57 -0300

    Atualiza deploy do rancher

--- scripts/.travis.sh
+++ scripts/.travis.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+
+export RANCHER_ENV=SEP/Organograma
+export RANCHER_SERVICE=organograma-api
+export RANCHER_START_FIRST=true
+export DOCKER_IMAGE=prodest/organograma-api
+export DOCKER_TAG=${TRAVIS_COMMIT:0:7}
 
 cd OrganogramaWebAPI/src/WebAPI/
 dotnet restore && dotnet publish -c release -r debian.8-x64 -o publish ./
