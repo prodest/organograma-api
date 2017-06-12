c9b9ff046d82243660281ae11682af335fd529b1
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-06-12 16:50:34 -0300

    Atualiza deploy do rancher

--- scripts/.release.sh
+++ scripts/.release.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-docker tag $DOCKER_IMAGE $DOCKER_IMAGE-hmg:$TRAVIS_COMMIT
+export RANCHER_STACK=prd
+export IMAGE_NAME=$DOCKER_IMAGE-hmg:$DOCKER_TAG
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $DOCKER_IMAGE-hmg:$TRAVIS_COMMIT
-
-export RANCHER_ENV_ID=1a10541 #env SEP/organograma (1a10541)
-export RANCHER_STACK_ID=1e102 #stack hmg (1e102)
-export RANCHER_STACK=hmg #stack hmg (1e102)
-
-export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID
-export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID/environments/$RANCHER_STACK_ID/composeconfig
-
-#Atualiza a infra
-git clone https://github.com/prodest/gerencio-upgrade.git
-cd gerencio-upgrade
-npm install
-node ./client $RANCHER_SERVICE_NAME 40000
+. ./scripts/.deploy.sh
