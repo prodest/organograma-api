eddf9598bd488a607e999aa6fa96192203a84b53
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-06-13 11:28:40 -0300

    Atualiza deploy do rancher

--- scripts/.develop.sh
+++ scripts/.develop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RANCHER_STACK=prd
+export RANCHER_STACK=dev
 export IMAGE_NAME=$DOCKER_IMAGE-dev:$DOCKER_TAG
 
 . ./scripts/.deploy.sh
--- scripts/.release.sh
+++ scripts/.release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RANCHER_STACK=prd
+export RANCHER_STACK=hmg
 export IMAGE_NAME=$DOCKER_IMAGE-hmg:$DOCKER_TAG
 
 . ./scripts/.deploy.sh
